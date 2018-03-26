8b10adc6094aa849ff966c33c4cac0c2a130ce6e
Karl Otto Olsen <Karl.Otto.Olsen@acando.no>
2018-03-26 12:55:17 +0200

    Added failure

--- ContinousWeb/Controllers/HomeController.cs
+++ ContinousWeb/Controllers/HomeController.cs
@@ -17,9 +17,10 @@ namespace ContinousWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            throw new NotImplementedException();
+            
+            // ViewData["Message"] = "Your application description page.";
+            //return View();
         }
 
         public IActionResult Contact()
